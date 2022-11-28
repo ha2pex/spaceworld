376822c7101a078249c0d3693f8effd194b1f84f
happex <110610727+ha2pex@users.noreply.github.com>
2022-11-29 00:33:47 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -2,14 +2,14 @@ function loadApp(path) {
 	var path = path;
 	const navbarContent = `
 	<ul>
-		<li><a href="${path}" class="navElem-L"><img src="${path}/static/assets/WSIcon.png" /></a></li>
-		<li><a href="${path}/get-started.html" class="navElem-L">Get started</a></li>
-		<li><a href="${path}/contact.html" class="navElem-L">Contact</a></li>
+		<li><a href="${path}"><img src="${path}/static/assets/WSIcon.png" /></a></li>
+		<li><a href="${path}/get-started.html">Get started</a></li>
+		<li><a href="${path}/contact.html">Contact</a></li>
 		<li class="dropdown">
-			<img src="${path}/static/icons/flag-en.svg" />
+			<a><img src="${path}/static/icons/flag-en.svg" /></a>
 			<ul class="submenu">
-				<li><a class="navElem-R"><img src="${path}/static/icons/flag-fr.svg" onClick="setLanguage('fr')" /></a></li>
-				<li><a class="navElem-R"><img src="${path}/static/icons/flag-uk.svg" onClick="setLanguage('uk')" /></a></li>
+				<li><a><img src="${path}/static/icons/flag-fr.svg" onClick="setLanguage('fr')" /></a></li>
+				<li><a><img src="${path}/static/icons/flag-uk.svg" onClick="setLanguage('uk')" /></a></li>
 			</ul>
 		</li>
 		<li><a class="navElem-R"><img src="${path}/static/icons/theme.svg" onClick="changeTheme()" /></a></li>
