a867de8a87df453c18445f9489da9dd65c893bd8
happex <110610727+ha2pex@users.noreply.github.com>
2022-11-29 23:24:16 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -2,10 +2,10 @@ function loadApp(path) {
 	var path = path;
 	const navbarContent = `
 	<div class="navbar">
-		<a class="left" href="#"><img class="logo" src="WSIcon.png" /></a>
-		<button><a class="left" href="#">Text 1</a></button>
-		<button><a class="left" href="#">Text 2</a></button>
-		<button><a class="right" href="#">Text 3</a></button>
+		<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
+		<button><a class="left" href="${path}/get-started.html">Get started</a></button>
+		<button><a class="left" href="${path}/contact.html">Contact us</a></button>
+		<button><a class="right" href="#"><img src="${path}/static/icons/flag-uk.svg" /></a></button>
 	</div>`
 	document.getElementById('navbar').innerHTML = navbarContent;
 }
