888704d6a7fa3b2625e8196e0efc1d0a92498759
happex <110610727+ha2pex@users.noreply.github.com>
2022-12-16 15:53:13 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -1,11 +1,19 @@
 function loadApp(path) {
 	var path = path;
-	const navbarContent = `
-	<div class="navbar">
-		<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
-		<button><a class="left" href="${path}/get-started.html">Get started</a></button>
-		<button><a class="left" href="${path}/contact.html">Contact us</a></button>
-		<button><a class="right" href="#"><img src="${path}/static/icons/flag-uk.svg" /></a></button>
-	</div>`
+	if (window.matchmedia("max-width: 480px").matches) {
+		const navbarContent = `
+		<div class="navbar">
+			<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
+			<button><a class="left" href="${path}/codex.html"><img src="${path}/static/assets/icons/book.svg" class="navbarIcon" /></a>
+			<button><a class="left" href="${path}/contact.html><img src="${path}/static/assets/icons/mail-open.svg" class="navbarIcon" /></a>
+		</div>`
+	} else {
+		const navbarContent = `
+		<div class="navbar">
+			<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
+			<button><a class="left" href="${path}/codex.html">Get started</a></button>
+			<button><a class="left" href="${path}/contact.html">Contact us</a></button>
+		</div>`
+	} 
 	document.getElementById('navbar').innerHTML = navbarContent;
 }
