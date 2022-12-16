925efb3109d4bdee156b1be483dbea5a7d222743
happex <110610727+ha2pex@users.noreply.github.com>
2022-12-16 15:59:08 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -1,19 +1,17 @@
 function loadApp(path) {
 	var path = path;
-	if (window.matchmedia("max-width: 480px").matches) {
-		const navbarContent = `
-		<div class="navbar">
-			<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
-			<button><a class="left" href="${path}/codex.html"><img src="${path}/static/assets/icons/book.svg" class="navbarIcon" /></a>
-			<button><a class="left" href="${path}/contact.html><img src="${path}/static/assets/icons/mail-open.svg" class="navbarIcon" /></a>
-		</div>`
-	} else {
-		const navbarContent = `
-		<div class="navbar">
-			<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
-			<button><a class="left" href="${path}/codex.html">Get started</a></button>
-			<button><a class="left" href="${path}/contact.html">Contact us</a></button>
-		</div>`
-	} 
+	const mNavbarContent = `
+	<div class="navbar">
+		<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
+		<button><a class="left" href="${path}/codex.html"><img src="${path}/static/assets/icons/book.svg" class="navbarIcon" /></a>
+		<button><a class="left" href="${path}/contact.html><img src="${path}/static/assets/icons/mail-open.svg" class="navbarIcon" /></a>
+	</div>`
+	const navbarContent = `
+	<div class="navbar">
+		<a class="left" href="${path}"><img class="logo" src="${path}/static/assets/WSIcon.png" /></a>
+		<button><a class="left" href="${path}/codex.html">Codex</a></button>
+		<button><a class="left" href="${path}/contact.html">Contact</a></button>
+	</div>`
+	document.getElementById('navbar-mobile").innerHTML = mNavbarContent;
 	document.getElementById('navbar').innerHTML = navbarContent;
 }
