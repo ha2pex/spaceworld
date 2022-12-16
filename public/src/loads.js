cfc4a6a31af6f4305b73c4474b6318356f74a3f3
happex <110610727+ha2pex@users.noreply.github.com>
2022-12-16 16:06:10 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -6,6 +6,7 @@ function loadApp(path) {
 		<button><a class="left" href="${path}/codex.html"><img src="${path}/static/assets/icons/book.svg" class="navbarIcon" /></a>
 		<button><a class="left" href="${path}/contact.html><img src="${path}/static/assets/icons/mail-open.svg" class="navbarIcon" /></a>
 	</div>`
+	document.getElementById('navbar-mobile").innerHTML = mNavbarContent;
 	
 	const navbarContent = `
 	<div class="navbar">
@@ -13,15 +14,13 @@ function loadApp(path) {
 		<button><a class="left" href="${path}/codex.html">Codex</a></button>
 		<button><a class="left" href="${path}/contact.html">Contact</a></button>
 	</div>`
+	document.getElementById('navbar').innerHTML = navbarContent;
 	
 	const footerContent = `
-	<a href="./guidelines.html">Guidelines</a><br>
-	<a href="./catalog/index.html">Catalog</a><br>
-	<a href="./privacy.html">Privacy</a><br><br>
+	<a href="./codex.html">Codex</a><br>
+	<a href="./catalog/index.html">Catalogue</a><br>
+	<a href="./privacy.html">Confidentialité</a><br><br>
 	<button class="btn btn-discord"><a href="https://discord.com" target="_blank">Discord</a></button>
-	<button class="btn btn-white"><a href="#header">Return to top</a></button>`
-	
-	document.getElementById('navbar-mobile").innerHTML = mNavbarContent;
-	document.getElementById('navbar').innerHTML = navbarContent;
-	document.getElementById('footer').innerHTML = footerContent;
+	<button class="btn btn-white"><a href="#header">Haut de page</a></button>`
+	document.getElementById('footer').innerHTML = footerContent;	
 }
