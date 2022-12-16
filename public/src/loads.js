3c96412c6f88343fd00a9c7980c5720129124452
happex <110610727+ha2pex@users.noreply.github.com>
2022-12-16 23:23:48 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -4,7 +4,7 @@ function loadApp(path) {
 	<div class="navbar">
 		<button><img src="${path}/static/icons/codex.svg" class="navbarIcon" /></button>
 		<button><img src="${path}/static/icons/mail-open.svg" class="navbarIcon" /></button>
-		<button><img src="${path}/static/assets/home.svg" class="navbarIcon" /></button>
+		<button><img src="${path}/static/icons/home.svg" class="navbarIcon" /></button>
 		<button><img src="${path}/static/icons/join.svg" class="navbarIcon" /></button>
 		<button><img src="${path}/static/icons/privacy.svg" class="navbarIcon" /></button>
 	</div>`
