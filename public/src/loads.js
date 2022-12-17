04a31e6d2f20bc01e1161451c40f638c4e4461d2
happex <110610727+ha2pex@users.noreply.github.com>
2022-12-17 14:22:53 +0100

    Update loads.js

--- public/src/loads.js
+++ public/src/loads.js
@@ -1,12 +1,13 @@
 function loadApp(path) {
 	var path = path;
+	var invite = 'https://discord.gg/Bp22t3qynX'
 	const mNavbarContent = `
 	<div class="navbar">
-		<button onClick="open('codex.html')"><img src="${path}/static/icons/codex.svg" class="navbarIcon" /></button>
-		<button onClick="open('contact.html')"><img src="${path}/static/icons/mail-open.svg" class="navbarIcon" /></button>
-		<button onClick="open('.')"><img src="${path}/static/icons/home.svg" class="navbarIcon" /></button>
+		<button onClick="open('${path}/codex.html')"><img src="${path}/static/icons/codex.svg" class="navbarIcon" /></button>
+		<button onClick="open('${path}/contact.html')"><img src="${path}/static/icons/mail-open.svg" class="navbarIcon" /></button>
+		<button onClick="open('${path}')"><img src="${path}/static/icons/home.svg" class="navbarIcon" /></button>
 		<button onClick="open('${invite}')"><img src="${path}/static/icons/join.svg" class="navbarIcon" /></button>
-		<button onClick="open('privacy.html')"><img src="${path}/static/icons/privacy.svg" class="navbarIcon" /></button>
+		<button onClick="open('${path}/privacy.html')"><img src="${path}/static/icons/privacy.svg" class="navbarIcon" /></button>
 	</div>`
 	document.getElementById('navbar-mobile').innerHTML = mNavbarContent;
 	
@@ -19,14 +20,14 @@ function loadApp(path) {
 	document.getElementById('navbar').innerHTML = navbarContent;
 	
 	const footerContent = `
-	<a href="./codex.html">Codex</a><br>
-	<a href="./catalog/index.html">Catalogue</a><br>
-	<a href="./privacy.html">Confidentialité</a><br><br>
-	<button class="btn btn-discord"><a href="https://discord.com" target="_blank">Discord</a></button>
+	<a href="${path}/codex.html">Codex</a><br>
+	<a href="${path}catalog/index.html">Catalogue</a><br>
+	<a href="${path}/privacy.html">Confidentialité</a><br><br>
+	<button class="btn btn-discord"><a href="${invite}" target="_blank">Discord</a></button>
 	<button class="btn btn-white"><a href="#header">Haut de page</a></button>`
 	document.getElementById('footer').innerHTML = footerContent;	
 }
 
 function open(link) {
-	window.location.replace = link;
+	location.replace(link);
 }
